8b374e6dd86f25cb4e231e69a1fa97c1fe1efb38
EugeneKolom <luxevgeniy@gmail.com>
2023-10-26 16:51:31 +0200

    make changing

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+      this.minRange = null;
+      this.maxRange = null;
+      this.currentGuess = null;
+    }
+  
     setRange(min, max) {
-
+      this.minRange = min;
+      this.maxRange = max;
     }
-
+  
     guess() {
-
+      this.currentGuess = Math.round((this.minRange + this.maxRange) / 2);
+      return this.currentGuess;
     }
-
+  
     lower() {
-
+      this.maxRange = this.currentGuess;
     }
-
+  
     greater() {
-
+      this.minRange = this.currentGuess;
     }
-}
+  }
 
 module.exports = GuessingGame;
